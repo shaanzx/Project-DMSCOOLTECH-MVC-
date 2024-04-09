9fe44159117cc1785ddaac7267e325634b969b03
Shanzx <anuhaslakshan07@gmail.com>
2024-04-09 14:41:26 +0530

    create model packge

--- src/main/java/lk/ijse/dmscooltech/Launcher.java
+++ src/main/java/lk/ijse/dmscooltech/Launcher.java
@@ -1,7 +1,21 @@
 package lk.ijse.dmscooltech;
 
-public class Launcher {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"))));
+
+        stage.setTitle("Login Form");
+        stage.centerOnScreen();
+        stage.show();
     }
 }
