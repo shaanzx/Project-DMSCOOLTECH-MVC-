9791385ca9e15d87ee186e3a34acab24bec020c1
Shanzx <anuhaslakshan07@gmail.com>
2024-04-14 17:39:10 +0530

    Login form and register form done

--- src/main/java/lk/ijse/dmscooltech/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dmscooltech/controller/LoginFormController.java
@@ -2,8 +2,23 @@ package lk.ijse.dmscooltech.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.dmscooltech.db.DbConnection;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Stack;
 
 public class LoginFormController {
 
@@ -11,7 +26,10 @@ public class LoginFormController {
     private PasswordField txtPassword;
 
     @FXML
-    private TextField txtUserName;
+    private CheckBox txtSave;
+
+    @FXML
+    private TextField txtUserId;
 
     @FXML
     void btnForgetOnAction(ActionEvent event) {
@@ -19,18 +37,62 @@ public class LoginFormController {
     }
 
     @FXML
-    void btnSignInAction(ActionEvent event) {
+    void btnSaveToDeviceOnAction(ActionEvent event) {
 
     }
 
     @FXML
-    void btnSignUpAction(ActionEvent event) {
+    void btnSignInAction(ActionEvent event) throws IOException {
+        String userId = txtUserId.getText();
+        String pw = txtPassword.getText();
 
+        try {
+            checkUserNamePassword(userId, pw);
+        } catch (SQLException e) {
+                new Alert(Alert.AlertType.ERROR,"User Id Or password dosen't match.Try agin!").show();
+        }
+    }
+
+    private void checkUserNamePassword(String userId, String pw) throws SQLException, IOException {
+        String sqlQuery = "SELECT uId, uPassword FROM user WHERE uId = ?";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+        preparedStatement.setObject(1,userId);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()){
+            String psw = resultSet.getString(2);
+            if(psw.equals(pw)){
+                gotoDashBoard();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Incorrect Password!");
+            }
+        }else{
+            new Alert(Alert.AlertType.INFORMATION,"Incorrect user ID.Check and try again.");
+        }
+    }
+
+    private void gotoDashBoard() throws IOException {
+        AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/dashBoard_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("DashBoard Form");
+        stage.show();
     }
 
     @FXML
-    void btnSignUpOnAction(ActionEvent event) {
+    void btnSignUpOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/register_form.fxml"));
 
+        Scene scene = new Scene(rootNode);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+
+        stage.setTitle("Register Form");
+        stage.show();
     }
-
 }
--- src/main/java/lk/ijse/dmscooltech/controller/RegisterFormControoler.java
+++ src/main/java/lk/ijse/dmscooltech/controller/RegisterFormControoler.java
@@ -1,4 +1,57 @@
 package lk.ijse.dmscooltech.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import lk.ijse.dmscooltech.db.DbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class RegisterFormControoler {
+
+    @FXML
+    private JFXButton txtConfirm;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtRePassword;
+
+    @FXML
+    private TextField txtUserId;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnConfirmOnAction(ActionEvent event) {
+        String uId = txtUserId.getText();
+        String uName = txtUserName.getText();
+        String password = txtPassword.getText();
+        String rePw = txtRePassword.getText();
+
+        userSave(uId,uName,password);
+    }
+
+    private void userSave(String uId, String uName, String password) {
+        try {
+            String sql = "INSERT INTO user VALUES(?, ?, ?)";
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setObject(1,uId);
+            preparedStatement.setObject(2,uName);
+            preparedStatement.setObject(3,password);
+
+            if(preparedStatement.executeUpdate() > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "User password Saved!").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR,"Error.Try again!").show();
+        }
+    }
 }
