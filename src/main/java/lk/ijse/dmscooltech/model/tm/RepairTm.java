aa10737f9b90aea6a56c0437ac1af2196943450d
Shanzx <anuhaslakshan07@gmail.com>
2024-04-29 19:26:33 +0530

    Update Repair FXML

---
+++ src/main/java/lk/ijse/dmscooltech/model/tm/RepairTm.java
@@ -0,0 +1,19 @@
+package lk.ijse.dmscooltech.model.tm;
+
+import com.jfoenix.controls.JFXButton;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class RepairTm {
+    private String vehicleNo;
+    private String employeeId;
+    private String description;
+    private double repairCost;
+    private String repairDate;
+    private double totalAmount;
+    private JFXButton delete;
+}
