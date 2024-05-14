83d9a421ff655995a92376b19d57dd05872bd377
Shanzx <anuhaslakshan07@gmail.com>
2024-05-14 13:25:28 +0530

    Create View Repair controller and FXML

---
+++ src/main/java/lk/ijse/dmscooltech/controller/ViewRepairFormController.java
@@ -0,0 +1,50 @@
+package lk.ijse.dmscooltech.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class ViewRepairFormController {
+
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colItemQty;
+
+    @FXML
+    private TableColumn<?, ?> colItemUnitPrice;
+
+    @FXML
+    private TableColumn<?, ?> colRepairCost;
+
+    @FXML
+    private TableColumn<?, ?> colRepairDate;
+
+    @FXML
+    private TableColumn<?, ?> colRepairId;
+
+    @FXML
+    private TableColumn<?, ?> colTotalAmount;
+
+    @FXML
+    private Pane orderViewPane;
+
+    @FXML
+    private TableView<?> tblRepairDetailsTable;
+
+    @FXML
+    private TextField txtRepairId;
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+}
